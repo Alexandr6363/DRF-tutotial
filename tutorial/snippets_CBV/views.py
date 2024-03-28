65e6e97b9b7a6617231aea74650dd5f9fb8311e2
alexr <0130200@gmail.com>
2024-03-28 15:52:35 +0400

    Step 4: Authentication & Permissions

--- tutorial/snippets_CBV/views.py
+++ tutorial/snippets_CBV/views.py
@@ -55,13 +55,32 @@ from rest_framework import status
 from snippets_CBV.models import Snippet
 from snippets_CBV.serializers import SnippetSerializer
 from rest_framework import generics
-
+from rest_framework import permissions
+from snippets_CBV.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)    
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
+
+from django.contrib.auth.models import User
+from snippets_CBV.serializers import UserSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("snippets.urls")),
     path('cbv/', include("snippets_CBV.urls")),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
